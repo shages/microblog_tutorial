cbf0a02552d34ff4b160241339c909b346a63b89
Erik Ronshagen <eronshagen@gmail.com>
2016-12-22 17:47:24 -0500

    part3 - login

--- app/views.py
+++ app/views.py
@@ -1,7 +1,26 @@
-
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html',
+    title="Log in silly!",
+    form=LoginForm())
 
 @app.route('/')
 @app.route('/index')
 def hello():
-    return "Hello World!"
+    user = {'nickname': 'Charlie'}
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, title="Yo yo yo",
+    posts=posts)
