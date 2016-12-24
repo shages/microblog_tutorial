45490803d53a8a9498d7b6f89447cf16857e0cab
Erik Ronshagen <eronshagen@gmail.com>
2016-12-23 22:27:51 -0500

    fix view and model import

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
-
 lm = LoginManager()
 lm.init_app(app)
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
