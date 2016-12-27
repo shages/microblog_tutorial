0003570cc8f55a1f7eef4c4fd4d6986440ce79c1
Erik Ronshagen <eronshagen@gmail.com>
2016-12-27 15:36:40 -0500

    Add unit tests

--- tests/flake.sh
+++ tests/flake.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 find ./app -name "*.py" -exec flake8 --ignore=E402,F401 {} +
+find ./tests -name "*.py" -exec flake8 --ignore=E402,F401 {} +
---
+++ tests/units.py
@@ -0,0 +1,82 @@
+"""Microblog unit tests."""
+
+import pytest
+import decorator
+import os
+import unittest
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from config import basedir
+from app import app, db
+from app.models import User
+
+
+@pytest.fixture
+def setup():
+    """Setup test fixture.
+
+    Sets Flask application config options, and creates a database.
+    """
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{0}'.format(
+        os.path.join(basedir, 'test.db'))
+    myapp = app.test_client()
+    db.create_all()
+    return myapp
+
+
+def td(func):
+    """Teardown decorator for test functions.
+
+    Removes database session.
+    """
+    def myfunc(func, *args, **kwargs):
+        try:
+            r = func(*args, **kwargs)
+        finally:
+            db.session.remove()
+            db.drop_all()
+        return r
+    return decorator.decorator(myfunc, func)
+
+
+#  -/- _   ,   -/- ,
+# _/__(/__/_)__/__/_)_
+
+
+def test_avatar():
+    """Test avatar function in User model class."""
+    u = User(nickname='john', email='john@example.com')
+    avatar = u.avatar(128)
+    expected = ('http://www.gravatar.com/avatar/'
+                'd4c74594d841139328695756648b6bd6')
+    assert avatar[0:len(expected)] == expected
+
+
+@td
+def test_make_unique_nickname(setup):
+    """Test unique user nickname creation."""
+    # Create single user
+    u = User(nickname='john', email='john@example.com')
+    db.session.add(u)
+    db.session.commit()
+
+    # Test make_unique_nickname() func
+    nickname = User.make_unique_nickname('john')
+    assert nickname != 'john'
+
+    # Create a second user with the new nickname
+    u = User(nickname=nickname, email='susan@example.com')
+    db.session.add(u)
+    db.session.commit()
+
+    # Create a second john user
+    nickname2 = User.make_unique_nickname('john')
+
+    # Shouldn't equal the original, nor the second user
+    assert nickname2 != 'john'
+    assert nickname2 != nickname
